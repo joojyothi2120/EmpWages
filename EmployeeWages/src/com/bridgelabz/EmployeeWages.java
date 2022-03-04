01a91b58e0a4686b2446a7ed7adb6bd0a9ae1fe7
jyothi <joojyothi2120@gmail.com>
2022-03-04 13:49:31 +0530

    Total Working Days

--- EmployeeWages/src/com/bridgelabz/EmployeeWages.java
+++ EmployeeWages/src/com/bridgelabz/EmployeeWages.java
@@ -2,26 +2,35 @@ package com.bridgelabz;
 
 public class EmployeeWages {
 
-	public static void main(String[] args) {
 
-
-		int isPartTimeHrs=8;
-		int empWage=0;
-		int empHrs=0;
-		int wagePerHr=20;
-		int isPartTime=1;
-		double empcheck=(int)Math.floor(Math.random() * 10 % 2);
-		if(empcheck == isPartTime)
-		 {
-			empHrs=8;
-		 }
-		else
-		 {
-			empHrs=0;
-		 }
-		empWage=(empHrs * wagePerHr);
-		System.out.println("empWage:"+empWage);
-
-	}
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
+    
+    public static void main(String[] args) {
+    int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+    while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <  NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+     int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+     switch (empCheck) {
+            case IS_PART_TIME:
+                 empHrs=4;
+                 break;
+            case IS_FULL_TIME:
+                 empHrs=8;
+                 break;
+            default:
+                 empHrs=0;
+    }
+    totalEmpHrs += empHrs;
+    System.out.println("Day:"+totalWorkingDays+"Emp Hr:"+empHrs);
+    }
+    int totalEmpWage = totalEmpHrs*EMP_RATE_PER_HOUR;
+    System.out.println("Total Emp Wage: " + totalEmpWage);
 
 }
+
+}
+
