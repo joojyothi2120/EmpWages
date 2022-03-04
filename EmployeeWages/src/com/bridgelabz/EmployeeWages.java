e97af55f155dabe249b0956095366930cb54e7b8
jyothi <joojyothi2120@gmail.com>
2022-03-04 13:36:12 +0530

    Using Switch Case

--- EmployeeWages/src/com/bridgelabz/EmployeeWages.java
+++ EmployeeWages/src/com/bridgelabz/EmployeeWages.java
@@ -1,21 +1,27 @@
 package com.bridgelabz;
 
 public class EmployeeWages {
+	
+		public static final int IS_FULL_TIME = 1;
+		  public static final int Wage_Per_Hr = 20;
+	          public static final int IS_PART_TIME = 2;
+		  public static void main(String args[]){
 
-	public static void main(String[] args) {
-
-		int is_Full_Time = 1;
-		double empCheck =(int)Math.floor(Math.random() * 10) % 2;
-		
-		if (empCheck == is_Full_Time)
-		{
-			System.out.println("Employee is Present");
-		}
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
-
+			int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+			int salary = 0;
+			switch (empCheck) {
+				case IS_FULL_TIME:
+				     empCheck=0;
+				     int empHrs=0;
+				     break;
+				case IS_PART_TIME:
+				     empHrs=4;
+				     break;
+				default:
+				     empHrs=0;
+	}
+			salary = empCheck * Wage_Per_Hr;
+			System.out.println("Employee Salary is="+salary+ "Rupees");
 	}
 
 }
